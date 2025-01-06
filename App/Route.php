c5038fb58223411ce7b724c5bb95518ccc6b1b83
JGabrielFDC <jgabrielfdc08@gmail.com>
2025-01-05 21:17:35 -0300

    -Instalação do Framework

--- App/Route.php
+++ App/Route.php
@@ -15,18 +15,6 @@ class Route extends Bootstrap
             "action" => "index"
         );
 
-        $routes["conteudo"] = array(
-            "route" => "/conteudo",
-            "controller" => "IndexController",
-            "action" => "conteudo"
-        );
-
-        $routes["sobre_nos"] = array(
-            "route" => "/sobre_nos",
-            "controller" => "IndexController",
-            "action" => "sobre_nos"
-        );
-
         $this->setRoutes($routes);
     }
 
